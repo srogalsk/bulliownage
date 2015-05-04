3b57fa13ba3c336a0cd775e47bfccbf3f0490a0f
Jesse <itzjesse@gmail.com>
2015-05-03 23:07:39 -0700

    merged

--- js/main.js
+++ js/main.js
@@ -68,8 +68,135 @@ function loadFooter(){
 
 
 
-// $(window).load(function() {
+$(window).load(function() {
+
+	var path = window.location.pathname;
+	var page = path.split("/").pop();
 
 
+	/* LETS MAKE A GRAPH */
 
-// });
+	var data = {
+		labels: ["January", "February", "March", "April", "May", "June", "July"],
+		datasets: [
+		{
+			label: "Gold Total",
+			fillColor: "rgba(104, 206, 222, 0.1)",
+			strokeColor: "#FF6D67",
+			pointColor: "rgba(104, 206, 222, 1)",
+			pointStrokeColor: "#fff",
+			pointHighlightFill: "#fff",
+			pointHighlightStroke: "#fff",
+			data: [700,820,700,800,730,950,900]
+		},
+		{
+			label: "Platinum Total",
+			fillColor: "rgba(104, 206, 222, 0.1)",
+			strokeColor: "#FFA859",
+			pointColor: "rgba(104, 206, 222, 1)",
+			pointStrokeColor: "#fff",
+			pointHighlightFill: "#fff",
+			pointHighlightStroke: "#fff",
+			data: [467, 555, 490, 550, 555, 560, 660]
+		},
+		{
+			label: "Silver Total",
+			fillColor: "rgba(151,187,205,0.2)",
+			strokeColor: "#F3FF88",
+			pointColor: "rgba(151,187,205,1)",
+			pointStrokeColor: "#fff",
+			pointHighlightFill: "#fff",
+			pointHighlightStroke: "#fff",
+			data: [200, 350, 300, 389, 330, 400, 488]
+		},
+		{
+			label: "1oz Gold",
+			fillColor: "rgba(151,187,205,0.2)",
+			strokeColor: "#9FFF98",
+			pointColor: "rgba(151,187,205,1)",
+			pointStrokeColor: "#fff",
+			pointHighlightFill: "#fff",
+			pointHighlightStroke: "#fff",
+			data: [100, 110, 120, 90, 102, 135, 115]
+		},
+		{
+			label: "1oz Platinum",
+			fillColor: "rgba(151,187,205,0.2)",
+			strokeColor: "#BBF5FF",
+			pointColor: "rgba(151,187,205,1)",
+			pointStrokeColor: "#fff",
+			pointHighlightFill: "#fff",
+			pointHighlightStroke: "#fff",
+			data: [56, 78, 67, 68, 73, 80, 76]
+		},
+		{
+			label: "1oz Silver",
+			fillColor: "rgba(151,187,205,0.2)",
+			strokeColor: "#C29FFF",
+			pointColor: "rgba(151,187,205,1)",
+			pointStrokeColor: "#fff",
+			pointHighlightFill: "#fff",
+			pointHighlightStroke: "#fff",
+			data: [20, 22, 20, 32, 35, 50, 40]
+		},
+		]
+	};
+
+	var options = {
+
+	    ///Boolean - Whether grid lines are shown across the chart
+	    scaleShowGridLines : true,
+
+	    //String - Colour of the grid lines
+	    scaleGridLineColor : "rgba(0,0,0,.05)",
+
+	    //Number - Width of the grid lines
+	    scaleGridLineWidth : 1,
+
+	    //Boolean - Whether to show horizontal lines (except X axis)
+	    scaleShowHorizontalLines: true,
+
+	    //Boolean - Whether to show vertical lines (except Y axis)
+	    scaleShowVerticalLines: true,
+
+	    //Boolean - Whether the line is curved between points
+	    bezierCurve : true,
+
+	    //Number - Tension of the bezier curve between points
+	    bezierCurveTension : 0.4,
+
+	    //Boolean - Whether to show a dot for each point
+	    pointDot : true,
+
+	    //Number - Radius of each point dot in pixels
+	    pointDotRadius : 4,
+
+	    //Number - Pixel width of point dot stroke
+	    pointDotStrokeWidth : 1,
+
+	    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+	    pointHitDetectionRadius : 20,
+
+	    //Boolean - Whether to show a stroke for datasets
+	    datasetStroke : true,
+
+	    //Number - Pixel width of dataset stroke
+	    datasetStrokeWidth : 2,
+
+	    //Boolean - Whether to fill the dataset with a colour
+	    datasetFill : true,
+
+	    //String - A legend template
+	    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
+
+	    responsive: true,
+
+	    maintainAspectRatio: false,
+
+	};
+
+	var ctx = document.getElementById("total-chart").getContext("2d");
+	var coinCart = new Chart(ctx).Line(data,options);
+	coinCart.update();
+
+});
