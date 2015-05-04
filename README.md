a0d6dbbcffdcc5b64c2102d3c7a145ead88afc29
Jesse <itzjesse@gmail.com>
2015-05-04 04:48:38 -0700

    fixed for safari

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# PowellPower
-134b dream team
+===============================================================================
+                             CSE134B DREAM TEAM
+                               COINFLIP - HW3
+===============================================================================
+
+# # # # # # # # # # # # # # # 
+#      Application Use      #
+# # # # # # # # # # # # # # #
+
+Navigation:
+	Start with index.html as the login/signup page. All other pages are named
+	as such:
+
+	index.html - Login/signup page with demo
+	wire2.html - Home page with Total Coin Value and graphs for all coins
+	wire3.html - My Gold page with details on owned gold coins
+	wire4.html - Gold Item page that looks at a specific gold coin
+	wire5.html - New Item page that is the mock-up for adding a new coin
+
+	Clicking on the AG (silver) or PT (platinum) boxes on the side nav will
+	simply navigate you to the AU (gold) page [wire3.html] as this mock-up
+	only implements the My Gold page.
+
+Responsive Design:
+	THe high-fidelity mock switches to mobile view when the screen size 
+	reaches a width of 1000px or below.
+
+
+# # # # # # # # # # # # # # # 
+#   Cross-Platform Issues   #
+# # # # # # # # # # # # # # #
+
+Chrome:
+	We mainly developed on Chrome, so there were issues with compatibility in
+	that department. 
+
+Firefox:
+	Firefox also seemed to work perfectly well with no issues. Did not note any
+	major problems, if any problems at all, with that browser. The only slight
+	difference that we noticed is that the dropdown selector in wire5.html was 
+	styled a little bit differently in Firefox (not as nice looking as we would
+	have hoped and seen in Chrome).
+
+Safari:
+	Here, the dropdown selector in wire5.html was also a little bit different, 
+	although not as ugly as in Firefox. One of the major issues we bumped into,
+	however, was that a lot of our SVG elements were not showing. This turned
+	out to be this really annoying problem that with SVG symbols, all other 
+	browsers let you call "use" before declaring the symbol EXCEPT Safari. 
+	This required an insane amount of browsing through all the files and 
+	doing a move of that line of code below the symbol declaration for pretty
+	much every single SVG element we used. Overall, testing on Safari brought
+	out the key point that some browsers will care about the order of certain
+	markup while others wont. 
+
+	Another thing we noticed is that Safari renders fonts a little bit 
+	differently from all the other browsers, and especially font-size. We used
+	font-size: 0 to bring some divs flush close to each other but on Safari
+	that doesn't work for some strange reason, so we compensated with some
+	white-space: nowrap so that our mobile toggling selectors didn't overflow.
+
+Internet Explorer:
+	
--- js/main.js
+++ js/main.js
@@ -1,41 +1,41 @@
 function loadTopNav(){
 	document.write("    <nav>");
-	document.write("        <svg class=\"icon-spinner2\"><use xlink:href=\"#icon-spinner2\"><\/use>");
+	document.write("        <svg class=\"icon-spinner2\">");
 	document.write("            <symbol id=\"icon-spinner2\" viewBox=\"0 0 1024 1024\">");
 	document.write("                <title>spinner2<\/title>");
 	document.write("                <path class=\"path1\" d=\"M1024 384h-384l143.53-143.53c-72.53-72.526-168.96-112.47-271.53-112.47s-199 39.944-271.53 112.47c-72.526 72.53-112.47 168.96-112.47 271.53s39.944 199 112.47 271.53c72.53 72.526 168.96 112.47 271.53 112.47s199-39.944 271.528-112.472c6.056-6.054 11.86-12.292 17.456-18.668l96.32 84.282c-93.846 107.166-231.664 174.858-385.304 174.858-282.77 0-512-229.23-512-512s229.23-512 512-512c141.386 0 269.368 57.326 362.016 149.984l149.984-149.984v384z\"><\/path>");
 	document.write("            <\/symbol>");
+	document.write("            <use xlink:href=\"#icon-spinner2\"><\/use>");
 	document.write("        <\/svg>");
 	document.write("        <a href=\"wire2.html\">COINFLIP<\/a>");
-	document.write("        <svg class=\"icon-cog\"><use xlink:href=\"#icon-cog\"><\/use>");
+	document.write("        <svg class=\"icon-cog\">");
 	document.write("            <symbol id=\"icon-cog\" viewBox=\"0 0 1024 1024\">");
 	document.write("                <title>cog<\/title>");
 	document.write("                <path class=\"path1\" d=\"M933.79 610.25c-53.726-93.054-21.416-212.304 72.152-266.488l-100.626-174.292c-28.75 16.854-62.176 26.518-97.846 26.518-107.536 0-194.708-87.746-194.708-195.99h-201.258c0.266 33.41-8.074 67.282-25.958 98.252-53.724 93.056-173.156 124.702-266.862 70.758l-100.624 174.292c28.97 16.472 54.050 40.588 71.886 71.478 53.638 92.908 21.512 211.92-71.708 266.224l100.626 174.292c28.65-16.696 61.916-26.254 97.4-26.254 107.196 0 194.144 87.192 194.7 194.958h201.254c-0.086-33.074 8.272-66.57 25.966-97.218 53.636-92.906 172.776-124.594 266.414-71.012l100.626-174.29c-28.78-16.466-53.692-40.498-71.434-71.228zM512 719.332c-114.508 0-207.336-92.824-207.336-207.334 0-114.508 92.826-207.334 207.336-207.334 114.508 0 207.332 92.826 207.332 207.334-0.002 114.51-92.824 207.334-207.332 207.334z\"><\/path>");
 	document.write("            <\/symbol>");
-	document.write("");
+	document.write("            <use xlink:href=\"#icon-cog\"><\/use>");
 	document.write("        <\/svg>");
 	document.write("    <\/nav>");
-	document.write("");
 }
 
 function loadTopNavPersist(){
 	document.write("    <nav style='display: block; visibility: visible;'>");
-	document.write("        <svg class=\"icon-spinner2\"><use xlink:href=\"#icon-spinner2\"><\/use>");
+	document.write("        <svg class=\"icon-spinner2\">");
 	document.write("            <symbol id=\"icon-spinner2\" viewBox=\"0 0 1024 1024\">");
 	document.write("                <title>spinner2<\/title>");
 	document.write("                <path class=\"path1\" d=\"M1024 384h-384l143.53-143.53c-72.53-72.526-168.96-112.47-271.53-112.47s-199 39.944-271.53 112.47c-72.526 72.53-112.47 168.96-112.47 271.53s39.944 199 112.47 271.53c72.53 72.526 168.96 112.47 271.53 112.47s199-39.944 271.528-112.472c6.056-6.054 11.86-12.292 17.456-18.668l96.32 84.282c-93.846 107.166-231.664 174.858-385.304 174.858-282.77 0-512-229.23-512-512s229.23-512 512-512c141.386 0 269.368 57.326 362.016 149.984l149.984-149.984v384z\"><\/path>");
 	document.write("            <\/symbol>");
+	document.write("            <use xlink:href=\"#icon-spinner2\"><\/use>");
 	document.write("        <\/svg>");
 	document.write("        <a href=\"wire2.html\">COINFLIP<\/a>");
-	document.write("        <svg class=\"icon-cog\"><use xlink:href=\"#icon-cog\"><\/use>");
+	document.write("        <svg class=\"icon-cog\">");
 	document.write("            <symbol id=\"icon-cog\" viewBox=\"0 0 1024 1024\">");
 	document.write("                <title>cog<\/title>");
 	document.write("                <path class=\"path1\" d=\"M933.79 610.25c-53.726-93.054-21.416-212.304 72.152-266.488l-100.626-174.292c-28.75 16.854-62.176 26.518-97.846 26.518-107.536 0-194.708-87.746-194.708-195.99h-201.258c0.266 33.41-8.074 67.282-25.958 98.252-53.724 93.056-173.156 124.702-266.862 70.758l-100.624 174.292c28.97 16.472 54.050 40.588 71.886 71.478 53.638 92.908 21.512 211.92-71.708 266.224l100.626 174.292c28.65-16.696 61.916-26.254 97.4-26.254 107.196 0 194.144 87.192 194.7 194.958h201.254c-0.086-33.074 8.272-66.57 25.966-97.218 53.636-92.906 172.776-124.594 266.414-71.012l100.626-174.29c-28.78-16.466-53.692-40.498-71.434-71.228zM512 719.332c-114.508 0-207.336-92.824-207.336-207.334 0-114.508 92.826-207.334 207.336-207.334 114.508 0 207.332 92.826 207.332 207.334-0.002 114.51-92.824 207.334-207.332 207.334z\"><\/path>");
 	document.write("            <\/symbol>");
-	document.write("");
+	document.write("            <use xlink:href=\"#icon-cog\"><\/use>");
 	document.write("        <\/svg>");
 	document.write("    <\/nav>");
-	document.write("");
 }
 
 function loadSideNav(selected){
@@ -46,12 +46,13 @@ function loadSideNav(selected){
 	else
 		document.write("        <figure>");
 	document.write("            <br\/>");
-	document.write("            <svg class=\"icon-home2\"><use xlink:href=\"#icon-home2\"><\/use>");
-	document.write("                <symbol id=\"icon-home2\" viewBox=\"0 0 1024 1024\">");
-	document.write("                    <title>home2<\/title>");
-	document.write("                    <path class=\"path1\" d=\"M426.667 853.333v-256h170.667v256h213.333v-341.333h128l-426.667-384-426.667 384h128v341.333z\"><\/path>");
-	document.write("                <\/symbol>");
-	document.write("            <\/svg>");
+	document.write("              <svg class=\"icon-home2\">");
+	document.write("                    <symbol id=\"icon-home2\" viewBox=\"0 0 1024 1024\">");
+	document.write("                        <title>home2<\/title>");
+	document.write("                        <path class=\"path1\" d=\"M426.667 853.333v-256h170.667v256h213.333v-341.333h128l-426.667-384-426.667 384h128v341.333z\"><\/path>");
+	document.write("                    <\/symbol>");
+	document.write("                    <use xlink:href=\"#icon-home2\"><\/use>");
+	document.write("                <\/svg>");
 	document.write("");
 	document.write("            <figcaption>Home<\/figcaption>");
 	document.write("        <\/figure>       ");
