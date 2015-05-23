42396ccdb91144f6a8cc7501b4fe068831afabf8
Steven Rogalsky <stevestar88@aol.com>
2015-05-23 16:01:00 -0700

    wire4 gets data from parse and fills elements

--- js/wire4.js
+++ js/wire4.js
@@ -10,17 +10,31 @@ var coin = new Coin();
 query.get("MjhMMmwzZJ", {
   success: function(coin) {
     // The object was retrieved successfully.
+    console.log("query success");
+
+    var months = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+
+    var metal = coin.get("metal");
+    var name = coin.get("name");
+    var purchasedAt = coin.get("purchasedAt");
+    var quantity = coin.get("quantity");
+    var premium = coin.get("premium");
+
+    $("#displaymetal").text(metal);
+    $("#displayname").text(name);
+    console.log(purchasedAt);
+    var date = purchasedAt.getDate();
+    var month = months[purchasedAt.getMonth()];
+    var year = purchasedAt.getFullYear();
+    $("#displaypurchasedate").text(date + " " + month + " " + year);
+    $("#displayquantity").text(quantity);
+    $("#displaypremium").text(premium);
+
   },
   error: function(object, error) {
     // The object was not retrieved successfully.
     // error is a Parse.Error with an error code and message.
   }
 });
-
-var metal = coin.get("metal");
-var name = coin.get("name");
-var purchasedAt = coin.get("purchasedAt");
-var quantity = coin.get("quantity");
-var premium = coin.get("premium");
-
-$("#displaymetal").text(metal);
