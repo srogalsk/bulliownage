5ef803640febd40468566df6c3d8c8d7f3f485f0
Chuyinzki <jesusvillegas3@gmail.com>
2015-06-04 12:31:17 -0700

    Added min 8 char password, password strength

--- js/signup.js
+++ js/signup.js
@@ -1,8 +1,55 @@
 Parse.initialize("lvKnEQfyaRezqqgnktnDZhTZQP3Yf9cpJV1lDXzf",
     "nKE6VI1LruKg7LMkpRmNin4IqldZfIYvE7KyyKCd");
 
+function scorePassword(pass) {
+    var score = 0;
+    if (!pass)
+        return score;
+
+    // award every unique letter until 5 repetitions
+    var letters = new Object();
+    for (var i=0; i<pass.length; i++) {
+        letters[pass[i]] = (letters[pass[i]] || 0) + 1;
+        score += 5.0 / letters[pass[i]];
+    }
+
+    // bonus points for mixing it up
+    var variations = {
+        digits: /\d/.test(pass),
+        lower: /[a-z]/.test(pass),
+        upper: /[A-Z]/.test(pass),
+        nonWords: /\W/.test(pass),
+    }
+
+    variationCount = 0;
+    for (var check in variations) {
+        variationCount += (variations[check] == true) ? 1 : 0;
+    }
+    score += (variationCount - 1) * 10;
+
+    return parseInt(score);
+}
+
+function checkPassStrength(pass) {
+    var score = scorePassword(pass);
+    if (pass.length < 8) return "";
+    if (score > 70)
+        return "strong";
+    if (score > 50)
+        return "good";
+    if (score >= 20)
+        return "weak";
+    return "";
+}
+
 $(document).ready(function () {
 
+    $("#passwordsignup").on("keypress keyup keydown", function() {
+        $("#strengthCheck").css('opacity', '1');
+        var pass = $(this).val();
+        $("#strength_human").text(checkPassStrength(pass));
+        $("#strength_score").text(scorePassword(pass));
+    });
 
     $("#fb-signup").on('click', function () {
 
@@ -49,6 +96,7 @@ $(document).ready(function () {
         //alert('signup button has been pressed');
         event.preventDefault();
         var user = new Parse.User();
+        var wePass = true;
 
         var username =  $("#usernamesignup").val();
         var email = $("#emailsignup").val();
@@ -59,8 +107,19 @@ $(document).ready(function () {
         user.set("email", email);
         user.set("password", pass);
         var passMatch = (pass == passconfirm);
-        if(passMatch){
 
+        if(!passMatch){
+            alert("Error: Your passwords do not match!");
+            wePass = false;
+        }
+
+        console.log(pass.length);
+        if(pass.length < 8){
+            alert("Error: Your passwords is not long enough! Minimum is 8 characters!");
+            wePass = false;
+        }
+
+        if(wePass){
             user.signUp(null, {
                 success: function (user) {
                     // Sign up successful!
@@ -97,9 +156,6 @@ $(document).ready(function () {
                     alert("Error: " + error.message);
                 }
             });
-        }
-        else{
-            alert("Error: Your passwords do not match!");
         }
     });
 
