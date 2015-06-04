eb853ec4f11843734c8a9be34d3bdd2302852ae9
Patrick <patthsu@gmail.com>
2015-06-04 15:23:25 -0700

    bunch of changes

--- parse/public/js/delete.js
+++
@@ -1,7 +0,0 @@
-function deleteCoin(id) {
-
-    var img_node = $("#" + id).children($(".trashcol")).children().children().get(1);
-    var span_node = $("#" + id).children($(".trashcol")).children().children().get(2);
-    img_node.style.display = "none";
-    span_node.style.display="inline";
-}
