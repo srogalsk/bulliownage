2de7c804bf5d0bffa0cdd4af4e22af975773b51e
Gevorg Aghabekov <gevorg.aghabekov@gmail.com>
2015-06-06 14:02:21 -0700

    Cleaning up code. Made facebook login clearer. Changed mouse on hover to hand icon instead of pointer.

--- js/main.js
+++ js/main.js
@@ -98,6 +98,45 @@ function loadFooter(){
 
 }
 
+function facebookLogin()
+{
+	Parse.initialize("lvKnEQfyaRezqqgnktnDZhTZQP3Yf9cpJV1lDXzf",
+		"nKE6VI1LruKg7LMkpRmNin4IqldZfIYvE7KyyKCd");
+
+	console.log("testing");
+
+	window.fbAsyncInit = function () {
+		Parse.FacebookUtils.init({ // this line replaces FB.init({
+			appId: 860624237308675, // Facebook App ID
+			status: true,  // check Facebook Login status
+			cookie: true,  // enable cookies to allow Parse to access the session
+			xfbml: true,  // initialize Facebook social plugins on the page
+			version: 'v2.3' // point to the latest Facebook Graph API version
+		});
+
+		// Run code after the Facebook SDK is loaded.
+		Parse.FacebookUtils.logIn(null, {
+			success: function (user) {
+				window.location.replace("wire2.html");
+			},
+			error: function (user, error) {
+				console.log("User cancelled the Facebook login or did not fully authorize.");
+			}
+		});
+	};
+
+
+	(function (d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s);
+		js.id = id;
+		js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=860624237308675";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+}
+
+
 function Get(yourPartialUrl, months){
     var d = new Date();
     d.setMonth(d.getMonth() - months);
